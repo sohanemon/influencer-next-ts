57309a8e61fa0135354c98b0be658bd75d9f2880
sohanemon <m.sohanemon@gmail.com>
2022-12-19 16:00:13 +0600

    checkbox fixed

--- app/login/form.tsx
+++ app/login/form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Input } from "@material-tailwind/react";
+import { Button, Checkbox, Input } from "@material-tailwind/react";
 
 export default function Form() {
   return (
@@ -16,6 +16,14 @@ export default function Form() {
         color='blue-gray'
         className=' h-10 !rounded-none [&~label]:before:rounded-tl-none [&~label]:after:rounded-tr-none !border-2'
       />
+      <br />
+      <div className='flex justify-between items-center'>
+        <Checkbox
+          className='checked:bg-primary checked:border-primary rounded-none border-2'
+          label='Remember Me'
+        />
+        <Button className='bg-primary rounded-none px-8'>Sign Up</Button>
+      </div>
     </section>
   );
 }
